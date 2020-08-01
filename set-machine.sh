85309906858791357aa7f57e425c0edf780dd8cd
Joel McCracken <mccracken.joel@gmail.com>
2020-08-01 02:24:57 -0400

    add some checks to the set-machine script

--- set-machine.sh
+++ set-machine.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [[ "$1" == "glamdring" ]] ; then
-    ln -s glamdring.nix this-machine.nix
+if [[ "$@" == "" ]] ; then
+    echo "you must provide a machine name"
+else
+    if [[ "$1" == "glamdring" ]] ; then
+      ln -s glamdring.nix this-machine.nix
+    else
+        echo "machine name unrecognized!"
+        exit 1
+    fi
 fi
