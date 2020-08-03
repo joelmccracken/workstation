a78e1ff61ea8ac985ac0b66257b77779435e1b2c
Joel McCracken <mccracken.joel@gmail.com>
2020-08-03 00:47:56 -0400

    i?????

--- link.sh
+++ link.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+ln --help
+
 # set up symlinks
+mkdir -p $HOME/.config/nixpkgs/home.nix
 echo ln -s `pwd`/home.nix $HOME/.config/nixpkgs/home.nix
 ln -s `pwd`/home.nix $HOME/.config/nixpkgs/home.nix
