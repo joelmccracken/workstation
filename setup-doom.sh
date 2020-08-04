a580a1f107a6f4655ed5aad287732b55976b96c8
Joel McCracken <mccracken.joel@gmail.com>
2020-08-04 01:50:22 -0400

    oh i see, nix was putting those files there...

--- setup-doom.sh
+++ setup-doom.sh
@@ -6,8 +6,10 @@ set -euo pipefail
 INSTALL_DIR=${DOOM_INSTALL_DIR:-~/.emacs.d/doom}
 BIN_DIR="${INSTALL_DIR}/bin"
 
-rm -rf ~/.emacs.d
-mkdir -p ~/.emacs.d
+# rm -rf ~/.emacs.d
+# mkdir -p ~/.emacs.d
+#
+ls -lah ~/.emacs.d/
 
 git clone --depth 1 https://github.com/joelmccracken/doom-emacs.git $INSTALL_DIR
 
