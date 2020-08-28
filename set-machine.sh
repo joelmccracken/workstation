fc7d4eaeb1b7d6ed25781fbc9947e8c833dde56b
Joel McCracken <mccracken.joel@gmail.com>
2020-08-27 22:16:12 -0400

    move machine configs to subdir

--- set-machine.sh
+++ set-machine.sh
@@ -4,16 +4,23 @@ set -euo pipefail
 
 if [[ "$@" == "" ]] ; then
     echo "you must provide a machine name"
-else
-    if [[ "$1" == "glamdring" ]] ; then
-      ln -s glamdring.nix this-machine.nix
-    else
-
-      if [[ "$1" == "gh-runner" ]] ; then
-        ln -s gh-runner.nix this-machine.nix
-      else
-        echo "machine name unrecognized!"
-        exit 1
-      fi
-    fi
+    exit 1
 fi
+
+if [[ "$1" == "glamdring" ]] ; then
+  ln -s machines/glamdring.nix this-machine.nix
+  exit 0
+fi
+
+if [[ "$1" == "gh-runner" ]] ; then
+  ln -s machines/gh-runner.nix this-machine.nix
+  exit 0
+fi
+
+if [[ "$1" == "sesco" ]] ; then
+  ln -s machines/sesco.nix this-machine.nix
+  exit 0
+fi 
+
+echo "machine name unrecognized!"
+exit 1
