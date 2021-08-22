a2b6a5a8ce5a44d13e3125aa6c3109b7918f2444
Joel McCracken <mccracken.joel@gmail.com>
2021-08-21 21:37:15 -0400

    foo

---
+++ Brewfile
@@ -0,0 +1,7 @@
+tap "railwaycat/emacsmacport"
+brew "emacs-mac", args: ["with-modules"]
+
+brew "git"
+brew "ripgrep"
+brew "coreutils" 
+brew "fd"
---
+++ new-setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
