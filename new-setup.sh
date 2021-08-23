f129f7c5deca885d024088094afdb75e29b0b822
Joel McCracken <mccracken.joel@gmail.com>
2021-08-22 21:07:13 -0400

    see if this will not be interactive

--- new-setup.sh
+++ new-setup.sh
@@ -8,4 +8,6 @@ echo $(pwd)
 brew bundle
 
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
-~/.emacs.d/bin/doom install
+# ~/.emacs.d/bin/doom install
+~/.emacs.d/bin/doom env
+~/.emacs.d/bin/doom sync
