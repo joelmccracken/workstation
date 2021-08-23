4edf9ca401054e3ae1653f21294df0236e58c933
Joel McCracken <mccracken.joel@gmail.com>
2021-08-22 22:42:25 -0400

    fix git url, also do checkout of "deleted" files

--- new-setup.sh
+++ new-setup.sh
@@ -6,9 +6,11 @@ brew bundle
 
 cd ~
 git init
-git remote add origin https://github.com/joelmccracken/real-dotfiles.git
+git remote add origin https://github.com/joelmccracken/workstation.git
 git fetch
 git reset --mixed origin/master
+
+git checkout $(git status -s | grep -E ' D' | awk '{print $2}')
 
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 yes | ~/.emacs.d/bin/doom install
