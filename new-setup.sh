b8f76fa514d8116e07549c3bc452ea9548783031
Joel McCracken <mccracken.joel@gmail.com>
2021-08-22 21:57:37 -0400

    foo

--- new-setup.sh
+++ new-setup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
-echo $(pwd)
-
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew bundle
 
+cd ~
+git init
+git remote add origin https://github.com/joelmccracken/real-dotfiles.git
+git reset --mixed origin/master
+
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 yes | ~/.emacs.d/bin/doom install
-# ~/.emacs.d/bin/doom
-# ~/.emacs.d/bin/doom sync
