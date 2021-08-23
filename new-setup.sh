b71d90e94cf4d5aa58b7757276026b18bc4e1b48
Joel McCracken <mccracken.joel@gmail.com>
2021-08-22 20:43:15 -0400

    smth

--- new-setup.sh
+++ new-setup.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo $(pwd)
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew bundle
+
+git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+~/.emacs.d/bin/doom install
