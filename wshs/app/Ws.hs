2f369045ee971bd64f778b3427a6d40505b660d7
Joel McCracken <mccracken.joel@gmail.com>
2022-01-02 16:14:57 -0500

    make it go

--- wshs/app/Ws.hs
+++ wshs/app/Ws.hs
@@ -1,26 +1,30 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 import Turtle
 import Data.Text (unpack)
 
-data Command = Check | Install | Remove
+data Command = Check | Install
   deriving (Show)
 
 parser :: Parser Command
 parser =   fmap (const Check)   (subcommand "check" "run system tests" $ pure ())
        <|> fmap (const Install) (subcommand "install" "install anything it can" $ pure ())
 
+main :: IO ()
 main = do
-  x <- options "workstation checker" parser
-  case x of
+  theOption <- options "workstation checker" parser
+  case theOption of
     Check -> check
     Install -> install
 
+check :: IO ()
 check = sh $ do
   file <- ls "/Users/jmccracken/check"
   liftIO $ print $ format fp file
   exitCode <- shell (format fp file) empty
   echo $ fromString $ "exit: " ++ show exitCode
 
+install :: IO ()
 install = sh $ do
   file <- ls "/Users/jmccracken/install"
   liftIO $ putStrLn $ "Executing: " ++ (unpack $ format fp file)
