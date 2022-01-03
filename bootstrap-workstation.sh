ee6034dd0ce6d8d314f7d70926c0ead505ac23e4
Joel McCracken <mccracken.joel@gmail.com>
2022-01-02 23:31:02 -0500

    attempt to make bootstrap idempotent and support linux

--- bootstrap-workstation.sh
+++ bootstrap-workstation.sh
@@ -11,17 +11,31 @@ fi
 
 WS_DIR="$HOME/workstation"
 
-# TODO should be naturally idempontent
-sudo bash -c '(xcodebuild -license accept; xcode-select --install) || exit 0'
+function is_mac() {
+    [[ "$(uname)" == "Darwin" ]]
+}
 
-# TODO first check to see if brew is installed
+function is_linux() {
+    [[ "$(uname)" == "Linux" ]]
+}
+
+is_mac && {
+    sudo bash -c '(xcodebuild -license accept; xcode-select --install) || exit 0'
+
+    which brew > /dev/null  || {
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+        # install git, necessary for next step
+        # TODO this should be safe to run even if its been run before
+        brew install git
+    }
+}
+
+is_linux && {
+    echo "TODO install git via apt"
+}
+
 # install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# install git, necessary for next step
-# TODO this should be safe to run even if its been run before
-brew install git
-
 polite-git-checkout () {
     DIR=$1
     REPO=$2
@@ -44,38 +58,41 @@ function mv_dir_dated_backup() {
     fi
 }
 
-cd ~
+{
+    cd ~;
+    [[ "$(git remote get-url origin)" == "git@github.com:joelmccracken/dotfiles.git" ]]
+} || polite-git-checkout ~ https://github.com/joelmccracken/dotfiles.git
 
-# TODO first check to see if origin etc is correct?
-polite-git-checkout ~ https://github.com/joelmccracken/dotfiles.git
 
-brew bundle
+is_mac && brew bundle
 
-# TODO check first to see if the desired contents have correct git
-#      remote for origin etc. Can manually fix if this check is not right
-mv_dir_dated_backup ~/workstation
-git clone https://github.com/joelmccracken/workstation.git
+{ cd ~/worksation
+     [[ "$(git remote get-url origin)" == "git@github.com:joelmccracken/workstation.git" ]]
+} || {
+    mv_dir_dated_backup ~/workstation
+    git clone https://github.com/joelmccracken/workstation.git
+}
 
-# TODO is there some way to see if doom is already installed??
-mv_dir_dated_backup ~/.emacs.d
-git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
-
-# If I ever have issues w/ this, I can use this form:
-# timeout 10m bash -c 'yes | ~/.emacs.d/bin/doom install' || exit 0
-~/.emacs.d/bin/doom -y install
-
-echo FINISHED INSTALLING DOOM
+{ cd ~/.emacs.d
+     [[ "$(git remote get-url origin)" == "https://github.com/hlissner/doom-emacs" ]]
+} || {
+    mv_dir_dated_backup ~/.emacs.d
+    git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+    # If I ever have issues w/ this, I can use this form:
+    # timeout 10m bash -c 'yes | ~/.emacs.d/bin/doom install' || exit 0
+    ~/.emacs.d/bin/doom -y install
+    echo FINISHED INSTALLING DOOM
+}
 
 echo installing nix
 
-# TODO see if nix is already installed
-sh <(curl -L https://nixos.org/nix/install)
+{ which nix > /dev/null } || sh <(curl -L https://nixos.org/nix/install)
 
 NIX_DAEMON_PATH='/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
-
 if [ -e "$NIX_DAEMON_PATH" ]; then
   (source "$NIX_DAEMON_PATH") || exit 0
 fi
 
-# TODO if stack is already installed, dont run this
-curl -sSL https://get.haskellstack.org/ | sh
+{ which stack > /dev/null } || {
+    curl -sSL https://get.haskellstack.org/ | sh
+}
