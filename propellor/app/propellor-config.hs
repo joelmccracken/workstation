20afbb1fed02029d0b65c171dc27ed83d55c9a72
Joel McCracken <mccracken.joel@gmail.com>
2022-02-06 14:52:48 -0500

    propellor config,

--- propellor/app/propellor-config.hs
+++ propellor/app/propellor-config.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -- This is the main configuration file for Propellor, and is used to build
 -- the propellor program.
 
@@ -9,9 +11,30 @@ import System.FilePath
 import Utility.UserInfo
 import Control.Monad.IO.Class
 import Data.Functor
+import Options.Applicative
+import qualified Data.Text as T
+
+data Options = Options { machineName :: T.Text }
+
+parseOptions :: Parser Options
+parseOptions =
+  Options <$> argument str (metavar "MACHINENAME")
+
+opts :: ParserInfo Options
+opts = info (parseOptions <**> helper)
+      ( fullDesc
+     <> progDesc "Print a greeting for TARGET"
+     <> header "hello - a test for optparse-applicative" )
 
 main :: IO ()
-main = mainProperties myHost
+main = do
+  options <- execParser opts
+  let theHost
+        = case machineName options of
+            "glamdring" -> glamdring
+            _ -> error $ T.unpack ("unknown host name " <>  machineName options)
+
+  mainProperties $ theHost
 
 -- TODO move this to wshs
 -- TODO try use haskell.nix
@@ -28,5 +51,5 @@ brewBundle = check (pure True :: IO Bool) $ theWork
         let bundleArgs = ["bundle", "--file", brewfile]
         cmdResult <$> boolSystem "brew" (map Param bundleArgs)
 
-myHost :: Host
-myHost = host "local" $ props & brewBundle
+glamdring :: Host
+glamdring = host "glamdring" $ props & brewBundle
