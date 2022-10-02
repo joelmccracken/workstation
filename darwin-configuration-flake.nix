c4d0e1e5e3caa533c5331a524198178103a70b16
Joel McCracken <mccracken.joel@gmail.com>
2022-10-01 22:11:11 -0400

    WIP

---
+++ darwin-configuration-flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "mJoel's darwin system";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-20.09-darwin";
+    darwin.url = "github:lnl7/nix-darwin/master";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, darwin, nixpkgs }: {
+    darwinConfigurations."Johns-MacBook" = darwin.lib.darwinSystem {
+      system = "x86_64-darwin";
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
