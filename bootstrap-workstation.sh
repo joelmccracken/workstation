4448e8a15906f0fcb0a9414576b28a14950b329e
Joel McCracken <mccracken.joel@gmail.com>
2022-10-06 00:03:29 -0400

    dont do darwin rebuild w flake

--- bootstrap-workstation.sh
+++ bootstrap-workstation.sh
@@ -151,9 +151,9 @@ is_mac && {
     nix-build https://github.com/LnL7/nix-darwin/archive/${NIX_DARWIN_VERSION}.tar.gz -A installer
     ./result/bin/darwin-installer
 
-    nix build ~/workstation\#darwinConfigurations.glamdring.system
-    ./result/sw/bin/darwin-rebuild switch --flake ~/workstation
-    rm -rf ./result
+    # nix build ~/workstation\#darwinConfigurations.glamdring.system
+    # ./result/sw/bin/darwin-rebuild switch --flake ~/workstation
+    # rm -rf ./result
 }
 
 cd  ~/workstation/wshs
