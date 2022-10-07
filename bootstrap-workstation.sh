875c2c8239a7e255a9f5570fe7ec090747b7d12b
Joel McCracken <mccracken.joel@gmail.com>
2022-10-06 20:39:12 -0400

    is it even possible for me to change the hostname?!??!

--- bootstrap-workstation.sh
+++ bootstrap-workstation.sh
@@ -158,9 +158,12 @@ is_mac && {
     restart_mac_daemon
 
     nix build ~/workstation\#darwinConfigurations.glamdring.system
-    bash -c 'echo hostname: $(hostname)'
+    sudo scutil --set ComputerName glamdring
+    sudo scutil --set HostName glamdring
+    sudo scutil --set LocalHostName glamdring
+    bash --login -c 'echo hostname: $(hostname)'
     # inside bash -c so the new hostname is picked up
-    bash -c './result/sw/bin/darwin-rebuild switch --flake ~/workstation'
+    bash --login -c './result/sw/bin/darwin-rebuild switch --flake ~/workstation'
     rm -rf ./result
 }
 
