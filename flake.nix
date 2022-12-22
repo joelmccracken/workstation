6ff04d056a058287faaa95199e8bb1c7a0566e9b
Joel McCracken <mccracken.joel@gmail.com>
2022-12-21 23:35:31 -0500

    flake lock

--- flake.nix
+++ flake.nix
@@ -2,17 +2,35 @@
   description = "Joel's darwin system";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     darwin.url = "github:lnl7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  outputs = { self, darwin, nixpkgs }: {
-    darwinConfigurations."glamdring" = darwin.lib.darwinSystem {
-      system = "x86_64-darwin";
-      modules = [ ./darwin-configuration.nix ];
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # darwinPackages = self.darwinConfigurations."glamdring".pkgs;
   };
+
+  outputs = { self, darwin, nixpkgs, home-manager }:
+    let
+      darwinPkgs = nixpkgs.legacyPackages."x86_64-darwin";
+    in {
+      darwinConfigurations."glamdring" = darwin.lib.darwinSystem {
+        system = "x86_64-darwin";
+        modules = [ ./darwin-configuration.nix ];
+      };
+
+      homeConfigurations.joel = home-manager.lib.homeManagerConfiguration {
+        inherit darwinPkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [
+          ./home.nix
+        ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
 }
