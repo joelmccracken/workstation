c9737bc11a68fa5732468bbf64f823ff549d92a5
Joel McCracken <mccracken.joel@gmail.com>
2022-12-22 01:09:50 -0500

    change back

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Joel's darwin system";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs";
     darwin.url = "github:lnl7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager = {
