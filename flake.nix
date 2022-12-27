74d76b6a7f1a0159bfdaa8fcebe97b18eb09dca1
Joel McCracken <mccracken.joel@gmail.com>
2022-12-26 21:04:08 -0500

    try it

--- flake.nix
+++ flake.nix
@@ -2,9 +2,16 @@
   description = "Joel's darwin system";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-22.05-darwin";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    darwin-nixpkgs = {
+      url = "github:nixos/nixpkgs/nixpkgs-22.05-darwin";
+    };
     darwin.url = "github:lnl7/nix-darwin/master";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    darwin.inputs.nixpkgs.follows = "darwin-nixpkgs";
+    darwin-home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "darwin-nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,27 +19,37 @@
   };
 
   outputs = { self, darwin, nixpkgs, home-manager }:
+    let
+      darwin-home-config = let
+          system = "x86_64-darwin";
+          pkgs = nixpkgs.legacyPackages.${system};
+        in home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+
+          modules = [
+            ./home.nix
+          ];
+      };
+      linux-home-config =
+        let
+          system = "x86_64-linux";
+          pkgs = nixpkgs.legacyPackages.${system};
+        in home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+
+          modules = [
+            ./home.nix
+          ];
+      };
+    in
     {
       darwinConfigurations."glamdring" = darwin.lib.darwinSystem {
         system = "x86_64-darwin";
         modules = [ ./darwin-configuration.nix ];
       };
 
-      homeConfigurations.joel =
-        let
-          system = "x86_64-darwin";
-          pkgs = nixpkgs.legacyPackages.${system};
-        in home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-
-          # Specify your home configuration modules here, for example,
-          # the path to your home.nix.
-          modules = [
-            ./home.nix
-          ];
-
-          # Optionally use extraSpecialArgs
-          # to pass through arguments to home.nix
-      };
+      homeConfigurations.glamdring.joel = darwin-home-config;
+      homeConfigurations."ci-macos".joel = darwin-home-config;
+      homeConfigurations."ci-ubuntu".joel = linux-home-config;
     };
 }
