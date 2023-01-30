c6271614b9e787c964651e705e581f47d17c8f09
Joel McCracken <mccracken.joel@gmail.com>
2023-01-29 19:08:34 -0500

    support setting current host directory

---
+++ hosts/glamdring/config.el
@@ -0,0 +1,20 @@
+;;; config.el --- Description -*- lexical-binding: t; -*-
+
+(after! org
+  (setq org-directory "~/Dropbox/EF/")
+  (setq org-roam-directory "~/Dropbox/EF/")
+  (setq org-roam-db-location "~/Dropbox/EF/org-roam.db")
+
+  (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
+  (setq org-directory "~/Dropbox/EF")
+  (setq org-id-locations-file "~/Dropbox/EF/.orgids.el")
+
+  (setq org-agenda-files '("~/Dropbox/EF"
+                           "~/Dropbox/EF/reference"
+                           "~/Dropbox/EF/projects"
+                           ))
+
+  (setq +org-capture-notes-file "inbox.org")
+  (setq org-mobile-files (org-agenda-files))
+  (setq org-mobile-inbox-for-pull "~/Dropbox/EF/inbox-mobile.org"))
+;;; config.el ends here
