83a291d6479e9593ef066e0b00790783a54cb28b
Joel McCracken <mccracken.joel@gmail.com>
2023-01-31 21:59:08 -0500

    .hmModule

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     darwin-nixpkgs = {
-      url = "github:nixos/nixpkgs/nixpkgs-22.05-darwin";
+      url = "github:nixos/nixpkgs/nixpkgs-22.11-darwin";
     };
     darwin.url = "github:lnl7/nix-darwin/master";
     darwin.inputs.nixpkgs.follows = "darwin-nixpkgs";
@@ -38,7 +38,7 @@
     };
   };
 
-  outputs = { self, darwin, nixpkgs, darwin-nixpkgs, home-manager, darwin-home-manager, nix-doom-emacs, darwin-nix-doom-emacs, ... }:
+  outputs = inputs@{ self, darwin, nixpkgs, darwin-nixpkgs, home-manager, darwin-home-manager, nix-doom-emacs, darwin-nix-doom-emacs, ... }:
     let
       darwin-home-config = let
           system = "x86_64-darwin";
@@ -47,9 +47,10 @@
           inherit pkgs;
 
           modules = [
-            darwin-nix-doom-emacs
+            darwin-nix-doom-emacs.hmModule
             ./home.nix
-          ];
+          ]
+;
         };
       linux-home-config =
         let
@@ -59,9 +60,10 @@
           inherit pkgs;
 
           modules = [
-            nix-doom-emacs
+            nix-doom-emacs.hmModule
             ./home.nix
           ];
+
         };
     in
     {
--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, nix-doom-emacs, ... }:
+{ config, pkgs, lib, ... }:
   {
     # Home Manager needs a bit of information about you and the
     # paths it should manage.
