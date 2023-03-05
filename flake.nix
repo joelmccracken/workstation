8ef6468c23df94578d4fdec13ea5f1df40baa85e
Joel McCracken <mccracken.joel@gmail.com>
2023-03-04 19:39:54 -0500

    Revert "Revert "merge home.nix into flake.nix""
    This reverts commit aac081cb7db0eae0dfb76c1685eee872d76c25e8.

--- flake.nix
+++ flake.nix
@@ -23,7 +23,61 @@
 
   outputs = inputs@{ self, darwin, nixpkgs, darwin-nixpkgs, home-manager, darwin-home-manager, nix-doom-emacs, # darwin-nix-doom-emacs,
     ... }:
+
     let
+      home-manager-config = { user, home }:
+        { config, pkgs, lib, ... }:
+          {
+            # Home Manager needs a bit of information about you and the
+            # paths it should manage.
+            home.username = user;
+            home.homeDirectory = home;
+
+            # This value determines the Home Manager release that your
+            # configuration is compatible with. This helps avoid breakage
+            # when a new Home Manager release introduces backwards
+            # incompatible changes.
+            #
+            # You can update Home Manager without changing this value. See
+            # the Home Manager release notes for a list of state version
+            # changes in each release.
+            home.stateVersion = "22.11";
+
+            # Let Home Manager install and manage itself.
+            programs.home-manager.enable = true;
+            home.packages = [
+              pkgs.git
+              pkgs.ripgrep
+              pkgs.jq
+              pkgs.jl
+              pkgs.fd
+              pkgs.ispell
+              pkgs.bitwarden-cli
+              pkgs.direnv
+              pkgs.mr  # myrepos https://myrepos.branchable.com/install/
+              pkgs.graphviz
+              pkgs.cmake
+              pkgs.coreutils
+              pkgs.wget
+            ];
+
+            home.sessionPath = [
+              "~/.nix-profile/bin/"
+            ];
+
+            programs.doom-emacs = {
+              enable = true;
+              doomPrivateDir = ./dotfiles/doom.d;
+              extraConfig = ''
+                (add-to-list 'exec-path "~/.nix-profile/bin/")
+              '';
+            };
+
+            # workaround; see https://github.com/nix-community/home-manager/issues/3342#issuecomment-1283158398
+            manual.manpages.enable = false;
+          };
+
+
       darwin-home-config = user-config:
         let
           system = "x86_64-darwin";
@@ -33,9 +87,8 @@
 
           modules = [
             nix-doom-emacs.hmModule
-            (import ./home.nix user-config)
-          ]
-;
+            (home-manager-config user-config)
+          ];
         };
       linux-home-config = user-config:
         let
@@ -46,7 +99,7 @@
 
           modules = [
             nix-doom-emacs.hmModule
-            (import ./home.nix user-config)
+            (home-manager-config user-config)
           ];
         };
       # nix-darwin-config: user-config:
