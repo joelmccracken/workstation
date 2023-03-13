c033fc20d149fec78bce8ad8232104ac2f199c46
Joel McCracken <mccracken.joel@gmail.com>
2023-03-13 00:07:32 -0400

    try embedding darwin config directly in flake.nix

--- flake.nix
+++ flake.nix
@@ -102,19 +102,58 @@
             (home-manager-config user-config)
           ];
         };
-      # nix-darwin-config: user-config:
+      nix-darwin-config = {hostname, user, ...}:
+        { config, pkgs, ... }:
+          {
+            networking.hostName = hostname;
+            networking.localHostName = hostname;
+
+            # List packages installed in system profile. To search by name, run:
+            # $ nix-env -qaP | grep wget
+            environment.systemPackages =
+              [ pkgs.vim
+              ];
+
+            # Auto upgrade nix package and the daemon service.
+            services.nix-daemon.enable = true;
+            nix.package = pkgs.nix;
+
+            # Create /etc/bashrc that loads the nix-darwin environment.
+            programs.zsh.enable = true;  # default shell on catalina
+            # programs.fish.enable = true;
+
+            # Used for backwards compatibility, please read the changelog before changing.
+            # $ darwin-rebuild changelog
+            system.stateVersion = 4;
+
+            nix.settings.trusted-users = ["root" user "runner"];
+            nix.settings.substituters = [
+              "https://cache.nixos.org"
+              "https://cache.iog.io"
+            ];
+            nix.settings.trusted-public-keys = [
+              "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+              "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+            ];
+
+            nix.extraOptions = ''
+            experimental-features = nix-command flakes
+            '';
+          }
     in
     {
       darwinConfigurations."glamdring" = darwin.lib.darwinSystem {
         system = "x86_64-darwin";
-        modules = [ ./darwin-configuration.nix ];
+        modules = [
+          (nix-darwin-config { user = "joel"; hostname = "glamdring"; })
+        ];
       };
-
       darwinConfigurations."ci-macos" = darwin.lib.darwinSystem {
         system = "x86_64-darwin";
-        modules = [ ./darwin-configuration.nix ];
+        modules = [
+          (nix-darwin-config { user = "runner"; hostname = "ci-macos"; })
+        ];
       };
-
       homeConfigurations.glamdring.joel = darwin-home-config {
         user = "joel"; home = "/Users/joel";
       };
