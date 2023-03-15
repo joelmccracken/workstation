0725ec0f426f46980c75828c71bef3b45fdd9dd3
Joel McCracken <mccracken.joel@gmail.com>
2023-03-14 23:46:26 -0400

    remove hardcoded username "joel"

--- bootstrap-workstation.sh
+++ bootstrap-workstation.sh
@@ -136,7 +136,7 @@ is_linux &&  {
 trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=
 substituters = https://cache.nixos.org https://cache.iog.io
 experimental-features = nix-command flakes
-trusted-users = root joel runner
+trusted-users = root $(whoami) runner
 build-users-group = nixbld
 # END OF /etc/nix/nix.conf
 EOF
