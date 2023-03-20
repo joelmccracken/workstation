5a98664726ee565b1f1507c95434a4dbe683404e
Joel McCracken <mccracken.joel@gmail.com>
2023-03-19 23:23:46 -0400

    extract helper funcs to lib, get secrets to ci run

--- lib/shell/funcs.sh
+++ lib/shell/funcs.sh
@@ -1,6 +1,22 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+# library of shell functions
 
+# [[file:../../workstation.org::*library of shell functions][library of shell functions:1]]
+function is_mac() {
+    [[ "$(uname)" == 'Darwin' ]]
+}
+
+function is_linux() {
+    [[ "$(uname)" == 'Linux' ]]
+}
+
+function info() {
+    echo "INFO ========= $(date) $@"
+}
+
+# unlocks bitwarden, so that the `bw` program can access the bitwarden database.
 bw_unlock () {
     # authtenticates bitwarden for this shell session only
     export BW_SESSION=`bw unlock --passwordfile ~/secrets/bw_pass --raw`;
 }
+# library of shell functions:1 ends here
