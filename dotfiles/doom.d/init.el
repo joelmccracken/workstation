d0ed84ead496e4aab5d63390e8172632e72df3c7
Joel McCracken <mccracken.joel@gmail.com>
2023-03-25 22:21:56 -0400

    racket

--- dotfiles/doom.d/init.el
+++ dotfiles/doom.d/init.el
@@ -158,7 +158,7 @@
        ;;purescript        ; javascript, but functional
        ;;python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
-       ;;racket            ; a DSL for DSLs
+       racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
