fbbc34d13bce8ca3140f3b8d21dac8250c19caad
Joel McCracken <mccracken.joel@gmail.com>
2023-11-05 22:39:16 -0500

    fix, set workstation_name before needed

--- bootstrap-workstation.sh
+++ bootstrap-workstation.sh
@@ -43,8 +43,6 @@ fi
 # [[file:../../workstation.org::workstation_foundation][workstation_foundation]]
 
 export WORKSTATION_DIR="$HOME/workstation"
-export WORKSTATION_HOST_SETTINGS_SRC_DIR=$WORKSTATION_DIR/hosts/$WORKSTATION_NAME
-export WORKSTATION_HOST_CURRENT_SETTINGS_DIR=$WORKSTATION_DIR/hosts/current
 export WORKSTATION_EMACS_CONFIG_DIR=~/.config/emacs
 export WORKSTATION_GIT_ORIGIN='git@github.com:joelmccracken/workstation.git'
 export WORKSTATION_GIT_ORIGIN_PUB='https://github.com/joelmccracken/workstation.git'
@@ -61,6 +59,17 @@ if [ -z "${WORKSTATION_NAME+x}" ] ; then
     fi
 fi
 
+if [ -z "${WORKSTATION_NAME+x}" ] ; then
+    echo WARNING: no environment variable WORKSTATION_NAME provided.
+    echo This variable should be exported by a script at:
+    echo ~/$WORKSTATION_DIR/hosts/current/settings.sh
+    echo see workstation.org for more information
+    echo TODO provide reference to exact location
+else
+    # only set these if we have a WORKSTATION_NAME. This may be a bad call, perhaps
+    export WORKSTATION_HOST_SETTINGS_SRC_DIR=$WORKSTATION_DIR/hosts/$WORKSTATION_NAME
+    export WORKSTATION_HOST_CURRENT_SETTINGS_DIR=$WORKSTATION_DIR/hosts/current
+fi
 # workstation_foundation ends here
 # These are the various versions of things that should be installed. Keeping them
 # in one place like this make them easier to keep track of.
--- lib/shell/foundation.sh
+++ lib/shell/foundation.sh
@@ -4,8 +4,6 @@
 
 # [[file:../../workstation.org::workstation_foundation][workstation_foundation]]
 export WORKSTATION_DIR="$HOME/workstation"
-export WORKSTATION_HOST_SETTINGS_SRC_DIR=$WORKSTATION_DIR/hosts/$WORKSTATION_NAME
-export WORKSTATION_HOST_CURRENT_SETTINGS_DIR=$WORKSTATION_DIR/hosts/current
 export WORKSTATION_EMACS_CONFIG_DIR=~/.config/emacs
 export WORKSTATION_GIT_ORIGIN='git@github.com:joelmccracken/workstation.git'
 export WORKSTATION_GIT_ORIGIN_PUB='https://github.com/joelmccracken/workstation.git'
@@ -20,5 +18,17 @@ if [ -z "${WORKSTATION_NAME+x}" ] ; then
     if [ -f "$WORKSTATION_HOST_CURRENT_SETTINGS_DIR/settings.sh" ]; then
        source "~/$WORKSTATION_HOST_CURRENT_SETTINGS_DIR/settings.sh"
     fi
+fi
+
+if [ -z "${WORKSTATION_NAME+x}" ] ; then
+    echo WARNING: no environment variable WORKSTATION_NAME provided.
+    echo This variable should be exported by a script at:
+    echo ~/$WORKSTATION_DIR/hosts/current/settings.sh
+    echo see workstation.org for more information
+    echo TODO provide reference to exact location
+else
+    # only set these if we have a WORKSTATION_NAME. This may be a bad call, perhaps
+    export WORKSTATION_HOST_SETTINGS_SRC_DIR=$WORKSTATION_DIR/hosts/$WORKSTATION_NAME
+    export WORKSTATION_HOST_CURRENT_SETTINGS_DIR=$WORKSTATION_DIR/hosts/current
 fi
 # workstation_foundation ends here
