7e2de9f2d7e32f56a398d2fd51a08a81ed0f90c4
Joel McCracken <mccracken.joel@gmail.com>
2023-11-15 00:43:36 -0500

    split initial bitwarden sync

--- bootstrap-workstation.sh
+++ bootstrap-workstation.sh
@@ -280,6 +280,33 @@ function install_nix_darwin() {
 
 # install_nix_darwin_function ends here
 
+# [[file:workstation.org::initial_bitwarden_sync_function][initial_bitwarden_sync_function]]
+# The initial BitWarden Sync process. Requires wshs/bww executable to
+# be built and available. This could all be more robust
+# extracting it is theoretically useful as it provides a mechanism for
+# resetting the secrets.
+# Likely this should be broken down into separate functions that can be reused.
+function initial_bitwarden_sync() {
+    # why is bash so cryptic
+    if [ ! -z "${BW_CLIENTID+x}" ] && \
+       [ ! -z "${BW_CLIENTSECRET+x}" ] && \
+       [ ! -z "${WS_BW_MASTER_PASS+x}" ]; then
+        info variables requried to run bww force-sync are set, running
+        if [ ! -d ~/secrets ]; then
+            mkdir ~/secrets;
+        fi
+        # overwriting anything that was previously in the file
+        echo "${WS_BW_MASTER_PASS}" > ~/secrets/bw_pass
+        bw login --apikey
+        bw_unlock
+        bw sync
+        $(nix path-info .#"wshs:exe:bww")/bin/bww force-sync
+    else
+        info variables required to run bww force sync are MISSING, skipping
+    fi
+}
+
+# initial_bitwarden_sync_function ends here
 info starting workstation bootstrap
 is_mac && {
     info ensuring xcode is installed
@@ -421,23 +448,7 @@ else
 fi
 
 
-# why is bash so cryptic
-if [ ! -z "${BW_CLIENTID+x}" ] && \
-   [ ! -z "${BW_CLIENTSECRET+x}" ] && \
-   [ ! -z "${WS_BW_MASTER_PASS+x}" ]; then
-    info variables requried to run bww force-sync are set, running
-    if [ ! -d ~/secrets ]; then
-        mkdir ~/secrets;
-    fi
-    # overwriting anything that was previously in the file
-    echo "${WS_BW_MASTER_PASS}" > ~/secrets/bw_pass
-    bw login --apikey
-    bw_unlock
-    bw sync
-    $(nix path-info .#"wshs:exe:bww")/bin/bww force-sync
-else
-    info variables required to run bww force sync are MISSING, skipping
-fi
+initial_bitwarden_sync
 
 
 cat <<-EOF
---
+++ lib/shell/setup/initial_bitwarden_sync.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# [[file:../../../workstation.org::*Initial Bitwarden Sync][Initial Bitwarden Sync:2]]
+# The initial BitWarden Sync process. Requires wshs/bww executable to
+# be built and available. This could all be more robust
+# extracting it is theoretically useful as it provides a mechanism for
+# resetting the secrets.
+# Likely this should be broken down into separate functions that can be reused.
+function initial_bitwarden_sync() {
+    # why is bash so cryptic
+    if [ ! -z "${BW_CLIENTID+x}" ] && \
+       [ ! -z "${BW_CLIENTSECRET+x}" ] && \
+       [ ! -z "${WS_BW_MASTER_PASS+x}" ]; then
+        info variables requried to run bww force-sync are set, running
+        if [ ! -d ~/secrets ]; then
+            mkdir ~/secrets;
+        fi
+        # overwriting anything that was previously in the file
+        echo "${WS_BW_MASTER_PASS}" > ~/secrets/bw_pass
+        bw login --apikey
+        bw_unlock
+        bw sync
+        $(nix path-info .#"wshs:exe:bww")/bin/bww force-sync
+    else
+        info variables required to run bww force sync are MISSING, skipping
+    fi
+}
+
+initial_bitwarden_sync
+# Initial Bitwarden Sync:2 ends here
