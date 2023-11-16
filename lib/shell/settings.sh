6ad80eb8ee73959237e1e486cb4eb1856530995f
Joel McCracken <mccracken.joel@gmail.com>
2023-11-16 00:28:13 -0500

    I think this broke thing?

--- lib/shell/settings.sh
+++ lib/shell/settings.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 # always avoid nix channels
-export NIX_PATH=""
+
+# export NIX_PATH=""
 
 export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig:${PKG_CONFIG_PATH}"
 export PKG_CONFIG_PATH="/usr/local/opt/libpq/lib/pkgconfig:${PKG_CONFIG_PATH}"
