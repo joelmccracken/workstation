35758dbf06753de052a3da62ecbcced95e20b4fb
Joel McCracken <mccracken.joel@gmail.com>
2023-11-16 20:06:58 -0500

    use a few more scripts on system

--- bootstrap-workstation.sh
+++ bootstrap-workstation.sh
@@ -258,28 +258,6 @@ function ensure_nix_installed () {
 }
 # ensure_nix_installed_function ends here
 
-# [[file:workstation.org::nix_darwin_rebuild_flake_function][nix_darwin_rebuild_flake_function]]
-function nix_darwin_rebuild_flake() {
-    nix build --extra-experimental-features "nix-command flakes" \
-        ~/workstation\#darwinConfigurations.${WORKSTATION_NAME}.system
-    ./result/sw/bin/darwin-rebuild switch --flake ~/workstation#${WORKSTATION_NAME}
-
-    rm -rf ./result
-}
-# nix_darwin_rebuild_flake_function ends here
-
-# [[file:workstation.org::install_nix_darwin_function][install_nix_darwin_function]]
-
-function install_nix_darwin() {
-    cd $WORKSTATION_DIR
-    nix-build https://github.com/LnL7/nix-darwin/archive/${WORKSTATION_NIX_DARWIN_VERSION}.tar.gz -A installer
-    ./result/bin/darwin-installer
-
-    nix_darwin_rebuild_flake
-}
-
-# install_nix_darwin_function ends here
-
 info starting workstation bootstrap
 is_mac && {
     info ensuring xcode is installed
@@ -364,7 +342,7 @@ is_linux && {
 
 is_mac && {
     info installing darwin-nix
-    install_nix_darwin
+    ~/workstation/lib/shell/setup/install_nix_darwin.sh
     info finished installing darwin-nix
 }
 
--- lib/shell/setup/install_nix_darwin.sh
+++ lib/shell/setup/install_nix_darwin.sh
@@ -1,23 +1,13 @@
 #!/usr/bin/env bash
-# [[file:../../../workstation.org::*Install nix-darwin][Install nix-darwin:2]]
+# Install nix-darwin
+
+# [[file:../../../workstation.org::*Install nix-darwin][Install nix-darwin:1]]
 source ~/workstation/lib/shell/foundation.sh
-source ~/workstation/lib/shell/setup/workstation_setup_verions.sh
+source ~/workstation/lib/shell/setup/workstation_setup_versions.sh
 
-function nix_darwin_rebuild_flake() {
-    nix build --extra-experimental-features "nix-command flakes" \
-        ~/workstation\#darwinConfigurations.${WORKSTATION_NAME}.system
-    ./result/sw/bin/darwin-rebuild switch --flake ~/workstation#${WORKSTATION_NAME}
+cd $WORKSTATION_DIR
+nix-build https://github.com/LnL7/nix-darwin/archive/${WORKSTATION_NIX_DARWIN_VERSION}.tar.gz -A installer
+./result/bin/darwin-installer
 
-    rm -rf ./result
-}
-
-function install_nix_darwin() {
-    cd $WORKSTATION_DIR
-    nix-build https://github.com/LnL7/nix-darwin/archive/${WORKSTATION_NIX_DARWIN_VERSION}.tar.gz -A installer
-    ./result/bin/darwin-installer
-
-    nix_darwin_rebuild_flake
-}
-
-install_nix_darwin
-# Install nix-darwin:2 ends here
+source ~/workstation/lib/shell/setup/nix-darwin-rebuild-flake.sh
+# Install nix-darwin:1 ends here
