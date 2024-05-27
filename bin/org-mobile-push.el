fe4b0fe31709a384e01642a7805776b88237fcf9
Joel McCracken <mccracken.joel@gmail.com>
2024-05-27 03:00:36 +0000

    fix

--- bin/org-mobile-push.el
+++ bin/org-mobile-push.el
@@ -2,4 +2,7 @@
 
 (require 'org)
 
+(org-mobile-pull)
+(save-some-buffers t)
 (org-mobile-push)
+(save-some-buffers t)
--- bin/update-ef-on-server.sh
+++ bin/update-ef-on-server.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
 #
 cd ~/EF
+git diff
 git add .
 git commit -m "$(date)"
 git fetch
 git reset --hard origin/main
 ~/.config/emacs/bin/doomscript ~/workstation/bin/org-mobile-push.el
+git diff
 git add .
 git commit -m "$(date)"
 git push origin main
